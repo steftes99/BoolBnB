a4d4edee5ce2d95fcfa7b8141774b04ad1f25d5a
steftes99 <stefanotescione83@gmail.com>
2021-12-13 11:20:26 +0100

    adjusted jumbotron

--- BoolBnb/resources/views/welcome.blade.php
+++ BoolBnb/resources/views/welcome.blade.php
@@ -3,13 +3,11 @@
 @section('content')
     <main>
         <div class="jumbotron bg_img d-flex align-items-center justify-content-center">
-            <div class="container d-flex flex-column justify-content-center">
+            <div class="container d-flex flex-column justify-content-center align-items-center">
                 <h3>BoolBnB</h3>
                 <h1>Prenota il tuo appartamento</h1>
                 <div class="input-group">
-                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                    aria-describedby="search-addon" />
-                    <button type="button" class="btn btn-primary mx-1">search</button>
+                    <button type="button" class="btn btn-primary btn-lg btn-block mx-1">I nostri appartamenti</button>
                 </div>
             </div>
             
