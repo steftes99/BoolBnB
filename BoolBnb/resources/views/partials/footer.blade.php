921db225a74ff78d61ff2738afa56b79d82d7395
steftes99 <stefanotescione83@gmail.com>
2021-12-15 11:55:25 +0100

    style dashboard and corrected footer

--- BoolBnb/resources/views/partials/footer.blade.php
+++ BoolBnb/resources/views/partials/footer.blade.php
@@ -1,15 +1,15 @@
-<footer class="position-relative fixed-bottom d-print-none mt-5">
-    <div class="py-6 bg-light text-muted">
-        <div class="container">
+<footer class="position-relative fixed-bottom mt-auto">
+    <div class="py-5 text-muted _my-bg">
+        <div class="container py-5">
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-lg-10">
                     <div class="fw-bold text-uppercase text-dark mb-3">
-                        <h6 class="text-uppercase text-dark mb-3">
+                        <h6 class="text-uppercase mb-3">
                             Lorem
                         </h6>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                    <ul class="list-inline">
+                    <ul class="list-inline _icons">
                         <li class="list-inline-item">
                             <a href="#" class="text-muted text-primary-hover">
                                 <i class="fab fa-twitter">
@@ -43,45 +43,45 @@
                     </ul>
                 </div>
                 <div class="col-lg-2 col-md-6 mb-5 mb-lg-10">
-                    <h6 class="text-uppercase text-dark mb-3">
+                    <h6 class="text-uppercase mb-3">
                         Lorem
                     </h6>
-                    <ul class="list-unstyled">
+                    <ul class="list-unstyled _my-list">
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="">Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="">Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="">Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="">Lorem</a>
                         </li>
                     </ul>
                 </div>
                 <div class="col-lg-2 col-md-6 mb-5 mb-lg-10">
-                    <h6 class="text-uppercase text-dark mb-3">
+                    <h6 class="text-uppercase mb-3">
                         Lorem
                     </h6>
-                    <ul class="list-unstyled">
+                    <ul class="list-unstyled _my-list">
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="" >Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="" >Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="" >Lorem</a>
                         </li>
                         <li>
-                            <a href="" class="text-muted">Lorem</a>
+                            <a href="" >Lorem</a>
                         </li>
                     </ul>
                 </div>
                 <div class="col-lg-4">
-                    <h6 class="text-uppercase text-dark mb-3">
+                    <h6 class="text-uppercase mb-3">
                         Lorem
                     </h6>
                     <p class="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
@@ -89,11 +89,11 @@
             </div>
         </div>
     </div>
-    <div class="py-4 fw-light bg-dark text-gray-300">
+    <div class="py-4 fw-light _bg-color text-gray-300">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-6 text-center text-md-start">
-                    <p class="text-sm mb-md-0 text-light">
+                    <p class="text-sm mb-md-0 _footer-sub">
                         © 2021, BoolBnB. All rights reserved.
                     </p>
                 </div>
--- BoolBnb/resources/views/welcome.blade.php
+++ BoolBnb/resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <main>
-        <div class="jumbotron bg_img d-flex align-items-center justify-content-center">
+        <div class=" bg_img d-flex align-items-center justify-content-center">
             <div class="container d-flex flex-column justify-content-center align-items-center">
-                <h3>BoolBnB</h3>
+                <h2>BoolBnB</h2>
                 <h1>Prenota il tuo appartamento</h1>
                 <div class="input-group">
                    
-                    <a type="button" href="{{ route('guest.apartments.index') }}" class="btn btn-primary btn-lg btn-block mx-1">I nostri appartamenti</a>
+                    <a type="button" href="{{ route('guest.apartments.index') }}" class="btn btn-dark btn-lg btn-block mx-1">I nostri appartamenti</a>
                 </div>
             </div>
             
