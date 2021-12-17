e8ab01dd2a631e3b027b0930a4e18e964d819433
steftes99 <stefanotescione83@gmail.com>
2021-12-17 12:33:04 +0100

    styling create

--- BoolBnb/resources/views/admin/apartments/create.blade.php
+++ BoolBnb/resources/views/admin/apartments/create.blade.php
@@ -1,78 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Inserisci un nuovo appartamento</h1>
-        @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+    <div class="background p-5">
+        <div class="container bg-light p-5 rounded-lg justify-content-center">
+            <h1 class="dark-gray">Inserisci un nuovo appartamento</h1>
+            @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            <form action="{{route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data" id="create-form">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Titolo</label>
+                    <input name="title" type="text" class="form-control" id="title" placeholder="Inserisci una breve descrizione">
                 </div>
-            @endif
-        <form action="{{route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data" id="create-form">
-            @csrf
-            <div class="form-group">
-                <label for="title">Titolo</label>
-                <input name="title" type="text" class="form-control" id="title" placeholder="Inserisci una breve descrizione">
-            </div>
-            <div class="form-group">
-                <label for="city">Città</label>
-                <input name="city" type="text" class="form-control" id="city" placeholder="Inserisci la città">
-            </div>
-            <div class="form-group">
-                <label for="region">Regione</label>
-                <input name="region" type="text" class="form-control" id="region" placeholder="Inserisci la regione">
-            </div>
-            <div class="form-group">
-                <label for="country">Stato</label>
-                <input name="country" type="text" class="form-control" id="country" placeholder="Inserisci lo stato">
-            </div>
-            <div class="form-group">
-                <label for="address">Indirizzo</label>
-                <input name="address" type="text" class="form-control" id="address" placeholder="Inserisci l'indirizzo">
-            </div>
-            <div class="form-group">
-                <label for="image">Inserisci l'immagine</label>
-                <input name="image" type="file" class="form-control-file" id="image">
-              </div>
-            <div class="form-group">
-                <label for="rooms">Stanze</label>
-                <input name="rooms" type="text" class="form-control" id="rooms" placeholder="Inserisci il numero di stanze">
-            </div>
-            <div class="form-group">
-                <label for="beds">Letti</label>
-                <input name="beds" type="text" class="form-control" id="beds" placeholder="Inserisci il numero di letti">
-            </div>
-            <div class="form-group">
-                <label for="bathrooms">Bagni</label>
-                <input name="bathrooms" type="text" class="form-control" id="bathrooms" placeholder="Inserisci il numero di bagni">
-            </div>
-            <div class="form-group">
-                <label for="square">Metri Quadri</label>
-                <input name="square" type="text" class="form-control" id="square" placeholder="Inserisci imetri quadri">
-            </div>
-            <div>
-                <h3>servizi</h3>
-                @foreach ($facilities as $facility)
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="facility-{{$facility->id}}" name="facilities[]" value="{{$facility->id}}">
-                    <label class="form-check-label" for="facility-{{$facility->id}}">{{ $facility->name}}</label>
+                <div class="form-group">
+                    <label for="city">Città</label>
+                    <input name="city" type="text" class="form-control" id="city" placeholder="Inserisci la città">
+                </div>
+                <div class="form-group">
+                    <label for="region">Regione</label>
+                    <input name="region" type="text" class="form-control" id="region" placeholder="Inserisci la regione">
+                </div>
+                <div class="form-group">
+                    <label for="country">Stato</label>
+                    <input name="country" type="text" class="form-control" id="country" placeholder="Inserisci lo stato">
+                </div>
+                <div class="form-group">
+                    <label for="address">Indirizzo</label>
+                    <input name="address" type="text" class="form-control" id="address" placeholder="Inserisci l'indirizzo">
+                </div>
+                <div class="form-group">
+                    <label for="image">Inserisci l'immagine</label>
+                    <input name="image" type="file" class="form-control-file" id="image">
                   </div>
-                @endforeach
-            </div>
-           
-
-            <div class="pt-3 text-center"> 
-                <button type="reset" class="btn btn-primary">Resetta</button>
-                <button type="submit" id="button-create" class="btn btn-primary">Crea</button>
-            </div>
-            
-            
-          </form>
+                <div class="form-group">
+                    <label for="rooms">Stanze</label>
+                    <input name="rooms" type="text" class="form-control" id="rooms" placeholder="Inserisci il numero di stanze">
+                </div>
+                <div class="form-group">
+                    <label for="beds">Letti</label>
+                    <input name="beds" type="text" class="form-control" id="beds" placeholder="Inserisci il numero di letti">
+                </div>
+                <div class="form-group">
+                    <label for="bathrooms">Bagni</label>
+                    <input name="bathrooms" type="text" class="form-control" id="bathrooms" placeholder="Inserisci il numero di bagni">
+                </div>
+                <div class="form-group">
+                    <label for="square">Metri Quadri</label>
+                    <input name="square" type="text" class="form-control" id="square" placeholder="Inserisci imetri quadri">
+                </div>
+                <div>
+                    <h3>servizi</h3>
+                    @foreach ($facilities as $facility)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="facility-{{$facility->id}}" name="facilities[]" value="{{$facility->id}}">
+                        <label class="form-check-label" for="facility-{{$facility->id}}">{{ $facility->name}}</label>
+                      </div>
+                    @endforeach
+                </div>
+               
+    
+                <div class="pt-3 text-center"> 
+                    <button type="reset" class="btn btn-primary _btn-color">Resetta</button>
+                    <button type="submit" id="button-create" class="btn btn-primary _btn-color">Crea</button>
+                </div>
+                
+                
+              </form>
+        </div>
     </div>
+    
 @endsection
 
 @section('scripts')
